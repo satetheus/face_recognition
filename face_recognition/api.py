a835d7b4d842155f1de78eace9280ca5bdc2c3be
satetheus <chriswarrenmccullough@gmail.com>
2020-05-01 22:08:30 -0600

    moved some redefinitions to imports

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -7,10 +7,10 @@ from PIL import ImageFile
 
 try:
     from face_recognition_models import (
-        pose_predictor_model_location,
-        pose_predictor_five_point_model_location,
-        cnn_face_detector_model_location,
-        face_recognition_model_location,
+        pose_predictor_model_location as predictor_68_point_model,
+        pose_predictor_five_point_model_location as predictor_5_point_model,
+        cnn_face_detector_model_location as cnn_face_detection_model,
+        face_recognition_model_location as face_recognition_model,
     )
 except Exception:
     print("Please install `face_recognition_models` with this command before",
@@ -21,18 +21,10 @@ except Exception:
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 face_detector = dlib.get_frontal_face_detector()
-
-predictor_68_point_model = pose_predictor_model_location()
-pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
-
-predictor_5_point_model = pose_predictor_five_point_model_location()
-pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
-
-cnn_face_detection_model = cnn_face_detector_model_location()
-cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
-
-face_recognition_model = face_recognition_model_location()
-face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
+pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model())
+pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model())
+cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model())
+face_encoder = dlib.face_recognition_model_v1(face_recognition_model())
 
 
 def _rect_to_css(rect):
